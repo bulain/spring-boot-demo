13c5e01a4374b217814560747bb382aff541faa3
Bulain Peng <bulain@gmail.com>
2016-11-30 00:10:41 +0800

    init spring boot demo

---
+++ src/main/java/com/bulain/main/Application.java
@@ -0,0 +1,22 @@
+package com.bulain.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
+        return factory;
+    }
+
+}
