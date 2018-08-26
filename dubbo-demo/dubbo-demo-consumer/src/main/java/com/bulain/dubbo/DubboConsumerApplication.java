b9d95396cdd82c35e70282b8ca541bedf8ed6d44
Bulain Peng <bulain@gmail.com>
2018-08-26 16:44:33 +0800

    update dubbo consumer application

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/bulain/dubbo/DubboConsumerApplication.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/bulain/dubbo/DubboConsumerApplication.java
@@ -3,7 +3,7 @@ package com.bulain.dubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class DubboConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DubboConsumerApplication.class, args);
