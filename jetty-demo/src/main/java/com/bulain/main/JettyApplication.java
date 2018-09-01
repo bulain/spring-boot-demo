c5e39aa535c2a3d65d72bcc08eca0217829e0fc8
Bulain Peng <bulain@gmail.com>
2018-09-01 22:33:12 +0800

    fix jetty demo

--- jetty-demo/src/main/java/com/bulain/main/JettyApplication.java
+++ jetty-demo/src/main/java/com/bulain/main/JettyApplication.java
@@ -2,21 +2,12 @@ package com.bulain.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JettyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JettyApplication.class, args);
-    }
-    
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
-        return factory;
     }
 
 }
