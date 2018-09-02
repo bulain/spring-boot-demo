3062460ba622e4c85cb1dacf400a917345089b0b
Bulain Peng <bulain@gmail.com>
2018-09-02 17:37:17 +0800

    simplify EnableCaching

--- shiro-demo/src/main/java/com/bulain/shiro/ShiroApplication.java
+++ shiro-demo/src/main/java/com/bulain/shiro/ShiroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableCaching(proxyTargetClass = true)
+@EnableCaching
 @EnableRedisHttpSession
 @SpringBootApplication
 public class ShiroApplication {
