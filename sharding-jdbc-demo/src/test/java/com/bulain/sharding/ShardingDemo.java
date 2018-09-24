e7715318ef6d57a9f3d447924d4722950326948e
Bulain Peng <bulain@gmail.com>
2018-09-24 23:52:43 +0800

    add generate keys

--- sharding-jdbc-demo/src/test/java/com/bulain/sharding/ShardingDemo.java
+++ sharding-jdbc-demo/src/test/java/com/bulain/sharding/ShardingDemo.java
@@ -2,7 +2,9 @@ package com.bulain.sharding;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
@@ -25,19 +27,19 @@ public class ShardingDemo {
 		conn.setAutoCommit(false);
 
 		for (int i = 1; i < 100; i++) {
-			PreparedStatement ps1 = conn
-					.prepareStatement("insert into t_order(order_id,user_id,status) values (?,?,?)");
-			ps1.setLong(1, i);
-			ps1.setInt(2, i % 10);
-			ps1.setString(3, "Y");
+			PreparedStatement ps1 = conn.prepareStatement("insert into t_order(user_id,status) values (?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps1.setInt(1, i % 10);
+			ps1.setString(2, "Y");
 			ps1.executeUpdate();
+			ResultSet generatedKeys = ps1.getGeneratedKeys();
+			generatedKeys.next();
+			long orderId = generatedKeys.getLong(0);
 
-			for (int j = 1; j < 9; j++) {
-				PreparedStatement ps2 = conn
-						.prepareStatement("insert into t_order_item(order_item_id,order_id,user_id) values (?,?,?)");
-				ps2.setLong(1, i * 10 + j);
-				ps2.setLong(2, i);
-				ps2.setInt(3, i % 10);
+			for (int j = 1; j < 10; j++) {
+				PreparedStatement ps2 = conn.prepareStatement("insert into t_order_item(order_id,user_id) values (?,?)");
+				ps2.setLong(1, orderId);
+				ps2.setInt(2, i % 10);
 				ps2.executeUpdate();
 			}
 			conn.commit();
