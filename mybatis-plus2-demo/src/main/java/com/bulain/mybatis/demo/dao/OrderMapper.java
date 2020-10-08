d4836ff8d2c411ab951901552d562b991fbd8f9e
Bulain Peng <bulain@gmail.com>
2020-10-08 19:37:33 +0800

    remove unused project

--- mybatis-plus2-demo/src/main/java/com/bulain/mybatis/demo/dao/OrderMapper.java
+++
@@ -1,8 +0,0 @@
-package com.bulain.mybatis.demo.dao;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.bulain.mybatis.demo.model.Order;
-
-public interface OrderMapper extends BaseMapper<Order>{
-
-}
