5b7a500a4534dd14e5d7d5bb565ec74f70e62fd7
Bulain Peng <bulain@gmail.com>
2021-01-28 23:01:22 +0800

    using redisson as redisTemplate

--- redis-demo/src/main/java/com/bulain/redis/RedisApplication.java
+++ redis-demo/src/main/java/com/bulain/redis/RedisApplication.java
@@ -2,13 +2,47 @@ package com.bulain.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 
 @SpringBootApplication
 public class RedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisSerializer<String> string = RedisSerializer.string();
+
+        ObjectMapper mapper = new ObjectMapper();
+        GenericJackson2JsonRedisSerializer.registerNullValueSerializer(mapper, null);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY);
+        mapper.setSerializationInclusion(Include.NON_NULL);
+        GenericJackson2JsonRedisSerializer json = new GenericJackson2JsonRedisSerializer(mapper);
+
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(string);
+        template.setValueSerializer(json);
+        template.setHashKeySerializer(string);
+        template.setHashValueSerializer(json);
+        template.setEnableDefaultSerializer(true);
+        template.setDefaultSerializer(json);
+
+        return template;
     }
 
 }
--- redis-demo/src/test/java/com/bulain/redis/RedissonDemo.java
+++ redis-demo/src/test/java/com/bulain/redis/RedissonDemo.java
@@ -1,13 +1,21 @@
 package com.bulain.redis;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,6 +25,8 @@ public class RedissonDemo {
 
     @Autowired
     private RedissonClient redissonClient;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     public void testRLock() {
@@ -30,6 +40,32 @@ public class RedissonDemo {
         } finally {
             lock.unlock();
         }
+    }
+
+    @Test
+    public void testOpsForValue() {
+        ValueOperations<String, Object> opsForValue = redisTemplate.opsForValue();
+        opsForValue.set("opsForValue1", "value1");
+        opsForValue.set("opsForValue2", "value2");
+
+        assertEquals("value1", opsForValue.get("opsForValue1"));
+        assertEquals("value2", opsForValue.get("opsForValue2"));
+    }
+
+    @Test
+    public void testOpsForItem() {
+        ValueOperations<String, Object> opsForValue = redisTemplate.opsForValue();
+        opsForValue.set("itemForValue1", new Item().setStr("value1"));
+
+        Item obj = (Item) opsForValue.get("itemForValue1");
+        assertEquals("value1", obj.getStr());
+    }
+
+    @Data
+    @Accessors(chain = true)
+    static class Item {
+        private String str;
+        private Date dt;
     }
 
 }
