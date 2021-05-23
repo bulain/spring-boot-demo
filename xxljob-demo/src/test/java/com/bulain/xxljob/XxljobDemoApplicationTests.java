88b1fe6ba49ec27a1c005dccb9a7ed19df72901b
Bulain Peng <bulain@gmail.com>
2021-05-23 19:22:40 +0800

    add xxljob demo

---
+++ xxljob-demo/src/test/java/com/bulain/xxljob/XxljobDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulain.xxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxljobDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
