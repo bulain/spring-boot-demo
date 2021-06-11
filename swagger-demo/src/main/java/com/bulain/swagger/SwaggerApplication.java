6425ee20e8da53a849ccbeecb643cc35181ef4a6
Bulain Peng <bulain@gmail.com>
2021-06-11 19:41:29 +0800

    upgrade core version

--- swagger-demo/src/main/java/com/bulain/swagger/SwaggerApplication.java
+++ swagger-demo/src/main/java/com/bulain/swagger/SwaggerApplication.java
@@ -3,14 +3,14 @@ package com.bulain.swagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @SpringBootApplication
 public class SwaggerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwaggerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
 
 }
