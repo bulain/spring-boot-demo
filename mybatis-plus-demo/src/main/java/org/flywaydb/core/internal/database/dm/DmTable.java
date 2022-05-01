e89dd611165c76df5de8a02e25716fb3cb11b187
Bulain Peng <bulain@gmail.com>
2022-05-02 00:00:47 +0800

    支持国产数据库达梦

---
+++ mybatis-plus-demo/src/main/java/org/flywaydb/core/internal/database/dm/DmTable.java
@@ -0,0 +1,38 @@
+package org.flywaydb.core.internal.database.dm;
+
+import org.flywaydb.core.internal.database.base.Table;
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * Oracle-specific table.
+ */
+public class DmTable extends Table<DmDatabase, DmSchema> {
+    /**
+     * Creates a new Oracle table.
+     *
+     * @param jdbcTemplate The Jdbc Template for communicating with the DB.
+     * @param database     The database-specific support.
+     * @param schema       The schema this table lives in.
+     * @param name         The name of the table.
+     */
+    public DmTable(JdbcTemplate jdbcTemplate, DmDatabase database, DmSchema schema, String name) {
+        super(jdbcTemplate, database, schema, name);
+    }
+
+    @Override
+    protected void doDrop() throws SQLException {
+        jdbcTemplate.execute("DROP TABLE " + database.quote(schema.getName(), name) + " CASCADE CONSTRAINTS PURGE");
+    }
+
+    @Override
+    protected boolean doExists() throws SQLException {
+        return exists(null, schema, name);
+    }
+
+    @Override
+    protected void doLock() throws SQLException {
+        jdbcTemplate.execute("LOCK TABLE " + this + " IN EXCLUSIVE MODE");
+    }
+}
---
+++ mybatis-plus-demo/src/main/resources/migration/dm/table/V20211018150546__create_table_demo_order.sql
@@ -0,0 +1,26 @@
+-- // create table demo_order
+-- Migration SQL that makes the change goes here.
+create table demo_order
+(
+    id           varchar(20) not null,
+
+    order_no     varchar(50) not null,
+    extn_ref_no1 varchar(50),
+    extn_ref_no2 varchar(50),
+    extn_ref_no3 varchar(50),
+
+    created_via  varchar(50),
+    remarks      varchar(500),
+    created_at   datetime,
+    created_by   varchar(50),
+    updated_at   datetime,
+    updated_by   varchar(50),
+    version      bigint       default 0,
+    deleted      int          default 0,
+    archived     char(1),
+
+    primary key (id)
+);
+
+create unique index idx_demo_order_1 on demo_order (order_no);
+
