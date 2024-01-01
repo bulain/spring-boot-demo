7640d5a75cb762db0f544a4f1baf471d43f424df
Bulain Peng <bulain@gmail.com>
2024-01-01 13:38:43 +0800

    整理项目依赖

--- mime-demo/src/main/java/com/bulain/mime/ctrl/MimeCtrl.java
+++ mime-demo/src/main/java/com/bulain/mime/ctrl/MimeCtrl.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
